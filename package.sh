d37104414614f985c6d7e349eafb13420c650432
20kdc <asdd2808@gmail.com>
2018-06-12 01:51:19 +0100

    Update packaging script so inst.lua version number can be easily checked

--- package.sh
+++ package.sh
@@ -8,8 +8,12 @@ lua claw/clawconv.lua code/data/app-claw/ < claw/code-claw.lua > /dev/null
 rm code.tar
 # Hey, look behind you, there's nothing to see here.
 # ... ok, are they seriously all named "Mann"?
-tar --owner=gray:0 --group=mann:0 -cf code.tar code
-lua heroes.lua `wc -c code.tar` | lua com2/bonecrunch.lua > inst.lua
+tar --mtime=0 --owner=gray:0 --group=mann:0 -cf code.tar code
+
+# Solely for ensuring that a -gold.lua file can be checked before being pushed to repository.
+echo -n "-- commit: " > inst.lua
+git status --porcelain=2 --branch | grep branch.oid >> inst.lua
+lua heroes.lua `wc -c code.tar` | lua com2/bonecrunch.lua >> inst.lua
 echo -n "--[[" >> inst.lua
 cat com2/code.tar.bd >> inst.lua
 echo -n "]]" >> inst.lua
